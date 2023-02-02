8766047fc12ded96f0dbe7616bd1dd01f6535420
TalonFox <105325988+TalonFox@users.noreply.github.com>
2023-02-02 17:18:38 -0600

    Almost done...
    Signed-off-by: TalonFox <105325988+TalonFox@users.noreply.github.com>

--- src/codegen-okami1041.lua
+++ src/codegen-okami1041.lua
@@ -15,6 +15,10 @@ return function(asmCode,astNodes)
 
     local functions = {}
     local allocated = {}
+    local saved = {}
+    local variables = {}
+    local loopCount = 1
+    local seenTempWarn = false
 
     local function ralloc()
         for i=0,7 do
@@ -23,11 +27,25 @@ return function(asmCode,astNodes)
                 return "t"..i
             end
         end
-        error("Register Overflow!")
+        if not seenTempWarn then
+            print("\x1b[1;33mWarning: Saved Registers have been allocated, concider simplifying your code.\x1b[0m")
+            seenTempWarn = true
+        end
+        for i=0,9 do
+            if not saved["s"..i] then
+                saved["s"..i] = true
+                return "s"..i
+            end
+        end
+        error("All registers are depleated!")
     end
 
     local function rfree(reg)
-        allocated[reg] = nil
+        if reg:sub(1,1) == "s" then
+            saved[reg] = nil
+        else
+            allocated[reg] = nil
+        end
     end
 
     local function getVal(arg,reg)
@@ -143,6 +161,64 @@ return function(asmCode,astNodes)
                 end
             end
         end,
+        ["!"] = function(args,reg)
+            getVal(args[1],reg)
+            local temp = ralloc()
+            rfree(temp)
+            text("    addi "..temp..", zero, -1\n")
+            text("    xor "..reg..", "..reg..", "..temp.."\n")
+        end,
+        ["&"] = function(args,reg)
+            for i,arg in ipairs(args) do
+                local argR = ralloc()
+                getVal(arg,argR)
+                rfree(argR)
+                if i == 1 then
+                    text("    add "..reg..", zero, "..argR.."\n")
+                else
+                    text("    and "..reg..", "..reg..", "..argR.."\n")
+                end
+            end
+        end,
+        ["|"] = function(args,reg)
+            for i,arg in ipairs(args) do
+                local argR = ralloc()
+                getVal(arg,argR)
+                rfree(argR)
+                if i == 1 then
+                    text("    add "..reg..", zero, "..argR.."\n")
+                else
+                    text("    or "..reg..", "..reg..", "..argR.."\n")
+                end
+            end
+        end,
+        ["^"] = function(args,reg)
+            for i,arg in ipairs(args) do
+                local argR = ralloc()
+                getVal(arg,argR)
+                rfree(argR)
+                if i == 1 then
+                    text("    add "..reg..", zero, "..argR.."\n")
+                else
+                    text("    xor "..reg..", "..reg..", "..argR.."\n")
+                end
+            end
+        end,
+        ["<"] = function(args,reg)
+            getVal(args[1],reg)
+            local r = ralloc()
+            rfree(reg)
+            getVal(args[2],r)
+            rfree(r)
+            text("    slt "..reg..", "..reg..", "..r.."\n")
+        end,
+        ["u<"] = function(args,reg)
+            getVal(args[1],reg)
+            local r = ralloc()
+            getVal(args[2],r)
+            rfree(r)
+            text("    sltu "..reg..", "..reg..", "..r.."\n")
+        end,
         ["w!"] = function(args)
             local addr = ralloc()
             getVal(args[1],addr)
@@ -152,6 +228,41 @@ return function(asmCode,astNodes)
             rfree(val)
             text("    sw "..val..", 0("..addr..")\n")
         end,
+        ["w@"] = function(args)
+            local addr = ralloc()
+            getVal(args[1],addr)
+            local val = ralloc()
+            rfree(addr)
+            getVal(args[2],val)
+            rfree(val)
+            text("    lw "..val..", 0("..addr..")\n")
+        end,
+        ["while"] = function(args)
+            local loopID = loopCount
+            loopCount = loopCount + 1
+            text(".VOSLoop"..loopID..":\n")
+            local condition = ralloc()
+            getVal(args[1],condition)
+            text("    beq "..condition..", zero, .VOSLoopAfter"..loopID.."\n")
+            rfree(condition)
+            for index=2,#args do
+                local i = args[index]
+                if functions[i.data.name] ~= nil then
+                    functions[i.data.name](i.data.nodes)
+                end
+            end
+            text("    b .VOSLoop"..loopID.."\n")
+            text(".VOSLoopAfter"..loopID..":\n")
+        end,
+        ["return"] = function(args)
+            if args[1] ~= nil then
+                local reg = ralloc()
+                getVal(args[1],reg)
+                rfree(reg)
+                text("    add a0, zero, "..reg.."\n")
+            end
+            text("    b .ret\n")
+        end
     }
 
     local function forEach(nodes,nodeType,fn)
@@ -182,11 +293,22 @@ return function(asmCode,astNodes)
     forEach(astNodes,"function",function(node)
         curArgs = node.data.args
         text(".global "..node.data.name..":\n")
+        variables = {}
+        for _,i in ipairs(node.data.nodes) do
+            if functions[i.data.name] == "auto" then
+                for _,varName in ipairs(i.data.nodes) do
+                    print(varName)
+                end
+                saved["_n"] = saved["_n"] + 1
+            end
+        end
+        saved["_n"] = nil
         for _,i in pairs(node.data.nodes) do
-            if functions[i.data.name] ~= nil then
+            if functions[i.data.name] ~= nil and functions[i.data.name] ~= "auto" then
                 functions[i.data.name](i.data.nodes)
             end
         end
+        text(".ret:\n")
         text("    blr zero, ra\n")
         allocated = {}
     end)
